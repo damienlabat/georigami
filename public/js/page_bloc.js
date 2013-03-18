2940a8f0b15ca048d6fe01abab39f61b879ee389
damienl <damien.labat@gmail.com>
2013-03-18 12:13:06 +0100

    add svg profil mouse events

--- public/js/page_bloc.js
+++ public/js/page_bloc.js
@@ -8,9 +8,6 @@ $(function() {
   Georigami.initBloc = function(view) {
 
 
-    console.log(view);
-
-
     var updateParams=function(face,vscale){
 
       if (face===null)   face=getBlocFace();
@@ -150,31 +147,34 @@ $(function() {
     if (option.dscale===null) option.dscale=0;
     if (option.vscale===null) option.vscale=1;
 
-    self=option;
+    profilobj=option;
 
-    self.changeStyle= function(newstyle) {
+    profilobj.changeStyle= function(newstyle) {
       svgobj.attr('class',newstyle);
     };
 
 
-    self.setSlicesTransform= function(dx,dy,dscale) {
+    profilobj.setSlicesTransform= function(dx,dy,dscale) {
 
       var slCount=  svgobj.find('g.gslice').length;
       var x;
       var y;
       var scale;
 
-      self.dx=dx;
-      self.dy=dy;
-      self.dscale=dscale;
+      var mouseXOnMouseDown;
+      var mouseYOnMouseDown;
+
+      profilobj.dx=dx;
+      profilobj.dy=dy;
+      profilobj.dscale=dscale;
 
       for (var id = 0; id< slCount; id++) {
         var obj=  svgobj.find('#slice'+id);
         var obj2= obj.find('g.gscale');
 
-        scale= 1- self.dscale * (((slCount-1)-id)/slCount);
-        x= (id-slCount/2) * self.dx;
-        y= (id-slCount/2) * self.dy;
+        scale= 1- profilobj.dscale * (((slCount-1)-id)/slCount);
+        x= (id-slCount/2) * profilobj.dx;
+        y= (id-slCount/2) * profilobj.dy;
 
         obj[0].setAttribute("transform",  "translate("+x+","+y+")");
         obj2[0].setAttribute("transform", "scale("+scale+")");
@@ -186,13 +186,72 @@ $(function() {
 
 
 
+     profilobj.onDocumentMouseDown= function( event ) {
+        event.preventDefault();
+
+        svgobj[0].addEventListener( 'mousemove', profilobj.onDocumentMouseMove, false );
+        svgobj[0].addEventListener( 'mouseup', profilobj.onDocumentMouseUp, false );
+       // svgobj[0].addEventListener( 'mouseout', profilobj.onDocumentMouseOut, false );
+
+        mouseXOnMouseDown = event.clientX ;
+        mouseYOnMouseDown = event.clientY ;
+
+      };
+
+      profilobj.onDocumentMouseMove= function( event ) {
+
+        mouseX = event.clientX ;
+        mouseY = event.clientY ;
+
+        profilobj.updateDxDy( (mouseX-mouseXOnMouseDown) /5000, (mouseY-mouseYOnMouseDown) /10000 );
+
+      };
+
+      profilobj.onDocumentMouseUp= function( event ) {
+
+        svgobj[0].removeEventListener( 'mousemove', profilobj.onDocumentMouseMove, false );
+        svgobj[0].removeEventListener( 'mouseup', profilobj.onDocumentMouseUp, false );
+        svgobj[0].removeEventListener( 'mouseout', profilobj.onDocumentMouseOut, false );
+
+      };
+
+      profilobj.onDocumentMouseOut= function( event ) {
+
+        svgobj[0].removeEventListener( 'mousemove', profilobj.onDocumentMouseMove, false );
+        svgobj[0].removeEventListener( 'mouseup', profilobj.onDocumentMouseUp, false );
+        svgobj[0].removeEventListener( 'mouseout', profilobj.onDocumentMouseOut, false );
+
+      };
+
+      profilobj.mousewheel= function( event ) {
+
+          event.preventDefault();
+          event.stopPropagation();
+
+          var delta = 0;
+          if ( event.wheelDelta ) { // WebKit / Opera / Explorer 9
+            delta = event.wheelDelta / 40;
+          } else if ( event.detail ) { // Firefox
+            delta = - event.detail / 3;
+          }
+
+          profilobj.updateDScale(delta/100);
+      };
+
+
+      svgobj[0].addEventListener( 'mousedown', profilobj.onDocumentMouseDown , false );
+      svgobj[0].addEventListener( 'mousewheel', profilobj.mousewheel, false );
+      svgobj[0].addEventListener( 'DOMMouseScroll', profilobj.mousewheel, false ); // firefox
+
+
+
 
     var updateCoords= function() {
 
       for (var id =0, len= data.coords.length; id < len; id++) {
         var slObj= svgobj.find('#slice'+id);
 
-        var svg_vscale= Georigami.svg_hscale*self.vscale;
+        var svg_vscale= Georigami.svg_hscale*profilobj.vscale;
         var coord='';
         for (var idCoord=0, lenCoord=data.coords[id].c.length; idCoord<lenCoord; idCoord++ ) {
 
@@ -215,17 +274,17 @@ $(function() {
 
     var updateViewBox= function() {
       var viewbox= { 'left':10000, 'right':-10000, 'top':10000, 'bottom':-10000 };
-      var svg_vscale= Georigami.svg_hscale*self.vscale;
+      var svg_vscale= Georigami.svg_hscale*profilobj.vscale;
 
       for (var id =0, len= data.coords.length; id < len; id++) {
 
-        var SCALE= 1-self.dscale*(((len-1)-id)/len);
+        var SCALE= 1-profilobj.dscale*(((len-1)-id)/len);
 
-        var bottom= (id-len/2) *self.dy + data.max/2*svg_vscale;
-        var top=    (id-len/2) *self.dy + data.max/2*svg_vscale - data.coords[id].m*svg_vscale*SCALE;
+        var bottom= (id-len/2) *profilobj.dy + data.max/2*svg_vscale;
+        var top=    (id-len/2) *profilobj.dy + data.max/2*svg_vscale - data.coords[id].m*svg_vscale*SCALE;
 
-        var left=   (id-len/2) *self.dx + data.dim/2*Georigami.svg_hscale - data.dim/2*Georigami.svg_hscale*SCALE;
-        var right=  (id-len/2) *self.dx + data.dim/2*Georigami.svg_hscale + data.dim/2*Georigami.svg_hscale*SCALE;
+        var left=   (id-len/2) *profilobj.dx + data.dim/2*Georigami.svg_hscale - data.dim/2*Georigami.svg_hscale*SCALE;
+        var right=  (id-len/2) *profilobj.dx + data.dim/2*Georigami.svg_hscale + data.dim/2*Georigami.svg_hscale*SCALE;
 
         if ( top < viewbox.top )        viewbox.top= top;
         if ( bottom > viewbox.bottom )  viewbox.bottom= bottom;
@@ -244,11 +303,9 @@ $(function() {
       viewbox.top=    viewbox.top    - height*0.05;
       viewbox.bottom= viewbox.bottom + height*0.05;
 
-      console.log(viewbox.bottom);
 
 
       var VB= viewbox.left + ' ' + viewbox.top + ' ' + (viewbox.right-viewbox.left) + ' ' + (viewbox.bottom-viewbox.top);
-      console.log(VB);
       svgobj[0].setAttribute( 'viewBox', VB );
 
     };
@@ -256,12 +313,24 @@ $(function() {
 
 
 
-    self.updateVScale= function(value) {
-      self.vscale= value;
+    profilobj.updateVScale= function(value) {
+      profilobj.vscale= value;
       updateCoords();
     };
 
-    return self;
+    profilobj.updateDxDy= function(dx,dy) {
+      $('#input-translateX').val( $('#input-translateX').val()*1+dx );
+      $('#input-translateY').val( $('#input-translateY').val()*1+dy );
+      $('#input-translateX').trigger('change');
+      $('#input-translateY').trigger('change');
+    }
+
+    profilobj.updateDScale= function(dscale) {
+      $('#input-scale').val( $('#input-scale').val()*1+dscale );
+      $('#input-scale').trigger('change');
+    }
+
+    return profilobj;
   }; // fin profil
 
 
