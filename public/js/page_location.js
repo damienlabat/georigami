2e6bdc8834a901502d9ec3e6954918f6bd4f323e
damienl <damien.labat@gmail.com>
2013-03-29 11:30:01 +0100

    debug js and restyle loc

--- public/js/page_location.js
+++ public/js/page_location.js
@@ -10,8 +10,7 @@ $(function() {
 
 
   function createGrid(map, data, marker) {
-    var gridObj=grid(map,data.lat,data.lng,data.width,data.height,data.vslices,data.hslices,data.rotate); 
-    console.log(data.rotate)        ;
+    var gridObj=grid(map,data.lat,data.lng,data.width,data.height,data.vslices,data.hslices,data.rotate);
       google.maps.event.addListener(marker, "mouseover", function() {
         gridObj.setOptions( { strokeColor: "#00FF00"  } );
       });
@@ -24,7 +23,7 @@ $(function() {
 
 
 
-    
+
      //initmap
 
     var Loc=new google.maps.LatLng (Georigami.location.lat , Georigami.location.lng );
@@ -34,8 +33,8 @@ $(function() {
         mapTypeId: google.maps.MapTypeId.TERRAIN
       };
 
-    map = new google.maps.Map(document.getElementById("map-canvas2"), mapOptions);  
-    
+    map = new google.maps.Map(document.getElementById("map-canvas2"), mapOptions);
+
 
     var markers = [];
     var bounds = new google.maps.LatLngBounds ();
@@ -61,24 +60,24 @@ $(function() {
         html =html+'<br/>';
 
         html =html+'altitude: '+Math.round(data.min)+'m to '+Math.round(data.max)+'m<br/>';
-        html =html+data.width+'m x '+data.height+'m<br/>';        
+        html =html+data.width+'m x '+data.height+'m<br/>';
         html =html+'slices: '+data.vslices+' x '+data.hslices+'<br/>';
         html =html+(data.vslices*data.vsamples+data.hslices*data.hsamples)+' samples<br/>';
         html =html+'created at  '+data.created_at+'<br/>';
 
         html =html+'</a>';
 
-      
+
         var marker = createMarker( html , new google.maps.LatLng(data.lat,data.lng), Georigami.baseurl+'/img/ico/'+Georigami.location.icon+'.png', Georigami.baseurl+'/img/ico/shadow.png' );
-      
-      gridObj=createGrid(map, data, marker);      
+
+      gridObj=createGrid(map, data, marker);
       bounds.union( gridObj.getBounds() );
-     
+
     }
 
-    map.fitBounds (bounds);    
+    map.fitBounds (bounds);
 
   }
 
 
-});  
+});
